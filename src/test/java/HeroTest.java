37cf2604da035e56542e092f6464859902c1faca
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-16 21:25:22 +0300

    Aded Hero hbs file

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
-class HeroTest {
+public class HeroTest {
 
     @BeforeEach
     void setUp() {
