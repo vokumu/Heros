d8a8415c525e602b3d1334b47be66fba1d958c40
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-16 18:22:25 +0300

    Added add Squads

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,7 +17,7 @@ public class App {
             model.put("squads",squads);
             return new ModelAndView(model, "squad.hbs");
         }, new HandlebarsTemplateEngine());
-        get("/addsquads", (request, response) -> { //request for route happens at this location
+        get("/addSquads", (request, response) -> { //request for route happens at this location
             Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
             return new ModelAndView(model, "addSquads.hbs"); // assemble individual pieces and render
         }, new HandlebarsTemplateEngine()); //
