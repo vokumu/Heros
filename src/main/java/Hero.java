1f9f74cfb11cbfecbaed2ebc9fb0cff4be24ce51
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-16 20:43:53 +0300

    Aded Hero constrctor

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+
 public class Hero {
     private String name;
     private int age;
     private String specialPower;
     private String weakness;
+
+    private static ArrayList<Squad> mInstances = new ArrayList<>();
+
+    //constructor
+    public Hero(String name,int age,String specialPower,String weakness){
+        this.name=name;
+        this.age=age;
+        this.specialPower=specialPower;
+        this.weakness=weakness;
+        mInstances.add(this);
+
+    }
+
+
+
 }
