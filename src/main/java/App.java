04536a4d7a563eedbf2c6c2cf3894c972ee03a82
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-16 15:34:18 +0300

    edited  the app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,19 @@
-public class App g{
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "index.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
+    }
 }
