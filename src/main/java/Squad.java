2738a971ca3153a647b4e6092e4078894d038491
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-16 15:04:16 +0300

    Added the squad class content

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Squad {
+    private static ArrayList<Squad> mInstances = new ArrayList<>();
+    private int maxSize;
+    private String name;
+    private String fightingCause;
+
+    public Squad(int maxSize,String name,String fightingCause){
+        this.maxSize=maxSize;
+        this.name=name;
+        this.fightingCause=fightingCause;
+        mInstances.add(this);
+
+    }
+    //get methods
+    public String getName() {
+        return name;
+    }
+    public int getMaxSize() { return maxSize;}
+    public String getFightingCause(){ return fightingCause;}
+    public static ArrayList<Squad> getAll() {
+        return mInstances;
+    }
+
 }
