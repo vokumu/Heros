ffed463b9b025cd3e80aabb2fc62a9a4aa4c31ba
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-18 14:25:08 +0300

    make it better

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,15 @@ import static java.lang.Integer.parseInt;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+          return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+      }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         get("/", (request, response) -> { //request for route happens at this location
             Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
